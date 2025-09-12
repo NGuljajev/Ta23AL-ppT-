665e3364bd53f8944220f8121ae8fb0a68745d69
NGuljajev <nguljajev@gmail.com>
2025-09-12 10:41:53 +0300

    forgot to add updated ad class, now added

--- Models/Ad.cs
+++ Models/Ad.cs
@@ -1,21 +1,41 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
-namespace Ta23ALõppTöö.Models
+[Table("ads")]
+public class Ad
 {
-    [Table("ads")]
-    public class Ad
-    {
-        public int CinemaId { get; set; }
-        public string Title { get; set; }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public long Id { get; set; }
 
-        public string ClientName { get; set; }
-        public string MediaUrl { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public string Placement { get; set; }
-        public decimal Price { get; set; }
-        public string Status { get; set; }
-        public DateTime CreatedAt { get; set; }
-    }
+    [Column("cinema_id")]
+    public int? CinemaId { get; set; }
+
+    [Column("title")]
+    public string Title { get; set; }
+
+    [Column("client_name")]
+    public string ClientName { get; set; }
+
+    [Column("media_url")]
+    public string MediaUrl { get; set; }
+
+    [Column("start_date")]
+    public DateTime StartDate { get; set; }
+
+    [Column("end_date")]
+    public DateTime EndDate { get; set; }
+
+    [Column("placement")]
+    public string Placement { get; set; }
+
+    [Column("price")]
+    public decimal Price { get; set; }
+
+    [Column("status")]
+    public string Status { get; set; }
+
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; }
 }
