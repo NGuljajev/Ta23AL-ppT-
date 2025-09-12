ff0b48c64b9c60754f37c04ecfbe805dfe9cde6f
Aleks Nazarenko <nazaren.aleks@gmail.com>
2025-09-12 11:22:05 +0300

    fixed ad controller error 500

--- AdDto.cs
+++ AdDto.cs
@@ -1,19 +1,44 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Ta23ALõppTöö.DTOs
 {
     public class AdDto
     {
         public long Id { get; set; }
-        public long? CinemaId { get; set; }
+
+        public long? CinemaId { get; set; } // optional foreign key
+
+        [Required]
+        [MaxLength(255)]
         public string Title { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string ClientName { get; set; }
+
+        [Required]
+        [MaxLength(500)]
         public string MediaUrl { get; set; }
+
+        [Required]
         public DateTime StartDate { get; set; }
+
+        [Required]
         public DateTime EndDate { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Placement { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
         public decimal Price { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Status { get; set; }
-        public DateTime CreatedAt { get; set; }
+
+        public DateTime CreatedAt { get; set; } // Usually set by DB, not required in POST
     }
 }
